650a113d9f8b1aa51ed8ac59083e78da8bcbde60
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-04 20:59:16 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub btnShow_Click(ByVal sender As Object, e As EventArgs) Handles btnShow.Click
+        Dim text1, text3 As String
+        Dim text2 As Integer
+        text1 = "Visual"
+        text2 = 22
+        'text3 = text1 + text2 ' InvalidCastexception 
+
+        'MsgBox(text3)
+
+        text3 = text1 & " " & text2 'ratify 
+        MsgBox(text3)
+
+
+
+
+
+    End Sub
+End Class
